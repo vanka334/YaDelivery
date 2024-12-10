002a328ac6f789c9558109f5d7f164862bfc71d3
vanka334 <113775267+vanka334@users.noreply.github.com>
2024-12-10 18:02:13 +0300

    Update PlacePhysicalDimensions.cs

--- YaDelivery/Models/ModelForGetPriceYa/PlacePhysicalDimensions.cs
+++ YaDelivery/Models/ModelForGetPriceYa/PlacePhysicalDimensions.cs
@@ -3,5 +3,8 @@
     public class PlacePhysicalDimensions
     {
         public int weight_gross { get; set; }
+        public string dx {get;set;}
+        public string dy {get; set:}
+        public string dz {get; set;}
     }
 }
